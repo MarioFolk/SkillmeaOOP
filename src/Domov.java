e559f92139c5485735162676cdf2541096f00ca8
Mario Folk <folkmario@gmail.com>
2025-11-05 23:04:25 +0100

    Metody - 3

--- src/Domov.java
+++ src/Domov.java
@@ -1,18 +1,19 @@
 
 public class Domov {
 
+    //Toto su vlastnosti
     float rozloha;
     int pocetOkien;
     String material;
     int pocetPoschodi;
     boolean jeAlarmZapnuty;
 
+    //Toto je konstruktor
+    Domov(){
+        System.out.println("Toto sa zavola pri vytvoreni noveho objektu");
+    }
 
-
-
-
-
-
+    //Toto je metoda
     public float vypocetCenyDomu(int lukrativnaOblast, String menoRodiny){
         System.out.println(menoRodiny);
         float vypocet = lukrativnaOblast*(pocetOkien*800 + rozloha*840);
