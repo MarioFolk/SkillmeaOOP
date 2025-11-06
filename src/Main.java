48562029f663c030d5c3ad631370d0ae6352b15f
Mario Folk <folkmario@gmail.com>
2025-11-06 02:07:16 +0100

    Uprava kodu

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,24 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Vytvorenie novych objektov z tiedy Domov
         Domov domNovakov = new Domov(536f, 10, 3);
         Domov domZajacov = new Domov(423f, 8, 2);
         Domov domKralikov = new Domov(356f, 6, 4);
 
         //System.out.println(domZajacov.rozloha + domNovakov.rozloha);
+
+        //Volanie metody vypocetCanyDomu
         float cenaDomuNovakov = domNovakov.vypocetCenyDomu(1, "Novakova");
         float cenaDomuZajacov = domZajacov.vypocetCenyDomu(3, "Zajacova");
         float cenaDomuKralikov = domKralikov.vypocetCenyDomu(2, "Kralikova");
 
+        //Volanie setter
         domNovakov.setPocetOkien(8);
+
+        //Volanie getter
         int pocetOkienNovakov = domNovakov.getPocetOkien();
+
         System.out.println(pocetOkienNovakov);
 
         System.out.println(cenaDomuZajacov + cenaDomuNovakov);
