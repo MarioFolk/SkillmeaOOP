9f0b988d34f7166371f6048086e37ed40647e6d1
Mario Folk <folkmario@gmail.com>
2025-11-06 02:23:42 +0100

    Pridanie triedy Obchod

---
+++ src/Obchod.java
@@ -0,0 +1,18 @@
+public class Obchod {
+    private float rozloha;
+    private int pocetPoschodi;
+    private int pocetPokladni;
+
+    Obchod(float rozloha, int pocetPoschodi, int pocetPokladni) {
+        this.rozloha = rozloha;
+        this.pocetPoschodi = pocetPoschodi;
+        this.pocetPokladni = pocetPokladni;
+
+    }
+
+
+
+
+
+
+}
